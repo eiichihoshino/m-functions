1279dde4d7122712e15c84c998edaab04a8fa674
eiichihoshino <eiichi.hoshino.m@gmail.com>
2016-06-27 17:26:36 +0900

    Change genpath to genpath with ignored paths

--- by_id3_add_ica.m
+++ by_id3_add_ica.m
@@ -3,7 +3,8 @@ input_path = 'by_id_check_valid';
 output_path = 'by_id_ica';
 %%%%%%%%%%%%%%%%%%
 
-addpath(genpath('.'));
+subpaths = regexp(genpath('.'), ';', 'split');
+addpath(strjoin(subpaths(cellfun(@(s) isempty(regexp(s, regexprep(strjoin({'cache', '.git'}, '|'), '\.', '\\.'))), subpaths)), ';'));
 if ~exist(output_path, 'dir')
     mkdir(output_path);
 end
