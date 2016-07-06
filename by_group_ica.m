d78ea3c7fbe1521da270ecfee0cd717c3106bc33
eiichihoshino <eiichi.hoshino.m@gmail.com>
2016-07-06 14:29:42 +0900

    Count up number of components with high contributed ch pairs

--- by_group_ica.m
+++ by_group_ica.m
@@ -12,23 +12,23 @@ for fig_i = 1:2
 end
 
 %aggregate ICA components by channels pairs
-threshold = 1;
+threshold = 1.5;
 fprintf('Aggregating ICA componentes:           ');
 for id_i = length(ids):-1:1
     fprintf([repmat('\b', [1 10]) '%s of %s'], zerofill(id_i, 3, ' '), zerofill(length(ids), 3, ' '));
     n_ch = length(ids(id_i).runs(1).connectivity.n);
-    ids(id_i).runs(1).connectivity.ica = NaN(n_ch,n_ch,4);
+    ids(id_i).runs(1).connectivity.ica = NaN(n_ch,n_ch,4); %the third axis: product max, product, min, number of +ch pairs more than threshold, number of -ch paris less than threshold 
     for ii = 1:n_ch-1
         for jj = ii+1:n_ch
             %aggregate ICA components by channels pairs
-            products = prod(ids(id_i).runs(1).ICA.A([ii jj], :));
-            ids(id_i).runs(1).connectivity.ica(ii, jj, 1:2) = [max(products) min(products)];
-            ids(id_i).runs(1).connectivity.ica(jj, ii, 1:2) = [max(products) min(products)];
+            %products = prod(ids(id_i).runs(1).ICA.A([ii jj], :));
+            %ids(id_i).runs(1).connectivity.ica(ii, jj, 1:2) = [max(products) min(products)];
+            %ids(id_i).runs(1).connectivity.ica(jj, ii, 1:2) = [max(products) min(products)];
             
             %count number of channel paris more than the threshold from standardized ICA components
             zscored_A = zscore(ids(id_i).runs(1).ICA.A);
             products = prod(zscored_A([ii jj], :));
-            ids(id_i).runs(1).connectivity.ica(ii, jj, 3:4) = [sum(products > threshold), sum(products < -threshold)];
+            ids(id_i).runs(1).connectivity.ica(ii, jj, 3:4) = [sum(products > threshold), sum(products < -threshold)]/size(ids(id_i).runs(1).ICA.A,2);
             ids(id_i).runs(1).connectivity.ica(jj, ii, 3:4) = ids(id_i).runs(1).connectivity.ica(jj, ii, 3:4);
         end
     end
@@ -36,7 +36,43 @@ end
 fprintf('\n');
 
 %ANOVA maximum absolute values of ICA's A channel products
-tests{end+1}.name = 'Anova ICA''s A channel products';
+tests{end+1}.name = 'Anova ICA''s A';
+fprintf('           ');
+for s_i = 4%:-1:1
+    fprintf([repmat('\b', [1 10]) '%s of %s'], zerofill(s_i, 3, ' '), zerofill(4, 3, ' '));
+    tests{end}.stats(s_i).ps =  NaN(n_ch);
+    tests{end}.stats(s_i).tbls = cell(n_ch);
+    for ii = 1:n_ch-1
+        for jj = ii+1:n_ch
+            [tests{end}.stats(s_i).ps(ii,jj), tests{end}.stats(s_i).tbls{ii,jj}] = anova1(arrayfun(@(x) x.runs(1).connectivity.ica(ii,jj,s_i), ids), [ids.GA_group], 'off');
+            tests{end}.stats(s_i).ps(jj,ii) = tests{end}.stats(s_i).ps(ii,jj);
+            tests{end}.stats(s_i).tbls{jj,ii} = tests{end}.stats(s_i).tbls{ii,jj};
+        end
+    end
+    
+    tests{end}.stats(s_i).ps(diag(true(1, n_ch))) = ones(1, n_ch);
+    p_values = tests{end}.stats(s_i).ps;
+    pfdrs = zeros(n_ch);
+    b = triu(true(n_ch), 1);
+    [~, ~, pfdrs(b)] = fdr_bh(p_values(b));
+    pfdrs = pfdrs + pfdrs' + diag(ones(1, n_ch));
+    tests{end}.stats(s_i).pfdrs = pfdrs;
+end
+fprintf('\n');
+name = 'anova_ica_a_channels_products';
+h = figure('Name', regexprep(name, '_', ' '), 'Visible', 'off');
+c = colormap;
+colormap(h, c(end:-1:1,:,:));
+s(1) = subplot(2,1,1);
+imagesc(tests{end}.ps, [0 0.1]);	colorbar;    xlabel('Channels');    ylabel('Channels'); title('p values');
+s(2) = subplot(2,1,2);
+imagesc(tests{end}.pfdrs, [0 0.8]);	colorbar;    xlabel('Channels');    ylabel('Channels'); title('FDR corrected p values');
+saveas(h, fullfile(figure_dir, ['anova_' name '.fig']));
+saveas(h, fullfile(figure_dir, ['anova_' name '.jpg']));
+close(h);
+    
+end
+fprintf('\n');
 tests{end}.ps = NaN(length(ids(1).runs(1).connectivity.n));
 tests{end}.tbls = cell(length(ids(1).runs(1).connectivity.n));
 for ii = 1:n_ch-1
@@ -46,7 +82,7 @@ for ii = 1:n_ch-1
         tests{end}.tbls{jj,ii} = tests{end}.tbls{ii,jj};
     end
 end
-tests{end}.ps(diag(true(1, m))) = ones(1, m);git
+tests{end}.ps(diag(true(1, m))) = ones(1, m);
 p_values = tests{end}.ps;
 [m, n] = size(p_values);
 pfdrs = zeros(m, n);
