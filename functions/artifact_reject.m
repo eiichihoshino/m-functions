76b4fcd4a3f53e798908983b6dc84a65fe887c7e
eiichihoshino <eiichi.hoshino.m@gmail.com>
2016-10-20 11:02:27 +0900

    Change not to use reshape. Add a parameter nan2true in artifact_reject.m. Refine format in load_data_by_run_from_csvs.m.

--- functions/artifact_reject.m
+++ functions/artifact_reject.m
@@ -1,24 +1,29 @@
-function new_data = artifact_reject(data, artifact_marks)
+function new_data = artifact_reject(data, artifact_marks, nan2true)
 %
 % ARTEFACT_REJECT: applies artifact_marks to data.
+% Ver. 1.0.5 Change not to use reshape. Add a parameter nan2true. 2016.10.19 Hoshino, E.
 %
 % INPUT
 % 	data: an array. This function acts along the first array dimension whose size does not equal 1.
 %	artifact_marks: a boolean vector a size agrees with a size of first some "data" dimensions.
+%   nan2false: treat NaN as this value. Default is true.
 %
 % OUTPUT
 % 	new_data: data without artifacts.
 %
-% Version 1.0.0.3 on 2016.6.1 by Hoshino, E..
-% Initialize.
+% [History]
+% Ver. 1.0.4 Change warning to error. 2016.10.12 Hoshino, E.
+% Ver. 1.0.3 Init. 2016.6.1 Hoshino, E.
 %
+if nargin < 3
+    nan2true = false;
+end
 sizes_of_data = size(data);
 if ~isequal(sizes_of_data(1:ndims(artifact_marks)), size(artifact_marks))
-    warning('Sizes of "artifact_marks" dimensions must agree with sizes of "data". Artifact_reject failed.');
-    return;
+    error('Sizes of "artifact_marks" dimensions must agree with sizes of "data". Artifact_reject failed.');
 end
-n_elements = numel(artifact_marks);
-artifact_marks_reshaped = reshape(artifact_marks, [n_elements 1]);
-data_reshaped = reshape(data, [n_elements numel(data)/n_elements]);
-data_reshaped(logical(artifact_marks_reshaped), :) = NaN;
-new_data = reshape(data_reshaped, sizes_of_data);
+
+artifact_marks(isnan(artifact_marks)) = nan2true;
+artifact_marks = logical(artifact_marks);
+new_data = data;
+new_data(repmat(artifact_marks, [ones([1 ndims(artifact_marks)]) sizes_of_data(ndims(artifact_marks)+1:end)])) = NaN;
--- functions/load_data_by_run_from_csvs.m
+++ functions/load_data_by_run_from_csvs.m
@@ -1,7 +1,7 @@
 function wave_struct=load_data_by_run_from_csvs(filenames)
 %
 % LOAD_DATA_BY_RUN_FROM_CSVS: loads and combines data from files.
-% Ver. 1.0.3 Add a line to ignore empty inputs. 2016.10.6 Hoshino, E.
+% Ver. 1.0.4 Refine format. 2016.10.20 Hoshino, E.
 %
 % INPUT
 % 	filenames: filepath to be loaded.
@@ -14,24 +14,25 @@ function wave_struct=load_data_by_run_from_csvs(filenames)
 % 	wave_struct.stim: stimulus marks.
 %
 % [History]
+% Ver. 1.0.3 Add a line to ignore empty inputs. 2016.10.6 Hoshino, E.
 % Ver. 1.0.2 on 2016.6.1 Hoshino, E.
 %
 filenames = filenames(~cellfun(@isempty, filenames));
 filename_heads = cellfun(@(x) x(1:end-5), filenames, 'UniformOutput', false);
 filename_heads = unique(filename_heads);
 if length(filename_heads) > 1
-	warning('Filenames we are going to combine are different. ');
+    warning('Filenames we are going to combine are different. ');
 end
 wave_structs = cell2mat(cellfun(@(x) load_data_from_csv(x), filenames, 'UniformOutput', false));
 [~,name_1,ext_1] = fileparts(filenames{1});
 for file_i  = 1:length(wave_structs)
-	[~,name_i,ext_i] = fileparts(filenames{file_i});
-	if ~isequal(wave_structs(1).datetime, wave_structs(file_i).datetime)
-		warning('"Mark" of %s is different from that of %s.', [name_i ext_i], [name_1 ext_1]);
-	end
-	if ~isequal(wave_structs(1).stim, wave_structs(file_i).stim)
-		warning('"Date" of %s is different from that of %s.', [name_i ext_i], [name_1 ext_1]);
-	end
+    [~,name_i,ext_i] = fileparts(filenames{file_i});
+    if ~isequal(wave_structs(1).datetime, wave_structs(file_i).datetime)
+        warning('"Mark" of %s is different from that of %s.', [name_i ext_i], [name_1 ext_1]);
+    end
+    if ~isequal(wave_structs(1).stim, wave_structs(file_i).stim)
+        warning('"Date" of %s is different from that of %s.', [name_i ext_i], [name_1 ext_1]);
+    end
 end
 wave_struct.filename = filename_heads{1};
 wave_struct.datetime = wave_structs(1).datetime;
@@ -48,40 +49,40 @@ wave_struct.stim = [];
 
 fid = fopen(filename);
 if fid == -1
-  warning('%s was not loaded. Check if it exist.', filename);
-  return
+    warning('%s was not loaded. Check if it exist.', filename);
+    return
 end
 
 fprintf('%s was', filename);
 index = 1;
 while ~feof(fid)
-	line = fgetl(fid);
-	if regexp(line, '^Date')
-		elems = regexp(line, ',', 'split');
-		wave_struct.datetime = elems{2};
-	elseif regexp(line, '^Wave\ Length')
-		wavelengths = regexp(line, '(?<=\().+?(?=\))', 'match');
-		wave_struct.wavelength = cell2mat(cellfun(@str2double, wavelengths, 'UniformOutput', false));
-	elseif regexp(line, '^Probe')
-		elems = regexp(line, ',', 'split');
-		column_index_of_mark = find(ismember(elems, 'Mark'));
-	elseif regexp(line, '^1,')
-		isSD = cellfun(@(x) isnan(str2double(x)), regexp(line, ',','split'), 'UniformOutput', false);
-		SD = {'%f', '%s'};
-		fseek(fid, 0, 'bof');
-		data = textscan(fid, [SD{cell2mat(isSD)+1}], 'Delimiter', ',', 'Headerlines', index-1);
+    line = fgetl(fid);
+    if regexp(line, '^Date')
+        elems = regexp(line, ',', 'split');
+        wave_struct.datetime = elems{2};
+    elseif regexp(line, '^Wave\ Length')
+        wavelengths = regexp(line, '(?<=\().+?(?=\))', 'match');
+        wave_struct.wavelength = cell2mat(cellfun(@str2double, wavelengths, 'UniformOutput', false));
+    elseif regexp(line, '^Probe')
+        elems = regexp(line, ',', 'split');
+        column_index_of_mark = find(ismember(elems, 'Mark'));
+    elseif regexp(line, '^1,')
+        isSD = cellfun(@(x) isnan(str2double(x)), regexp(line, ',','split'), 'UniformOutput', false);
+        SD = {'%f', '%s'};
+        fseek(fid, 0, 'bof');
+        data = textscan(fid, [SD{cell2mat(isSD)+1}], 'Delimiter', ',', 'Headerlines', index-1);
+    end
     if ~isempty(wave_struct.wavelength)
-      wave_struct.data = [data{1, 2:length(wave_struct.wavelength)+1}];
-		else
-			warning('A line start with "Wave Length" was not found.');
-		end	
-		if exist('column_index_of_mark', 'var') && ~isempty(column_index_of_mark)
-			wave_struct.stim = data{:, column_index_of_mark};
-		else
-			warning('A column name "Mark" was not found in a line start with Probe."');
-		end
-	end
-	index = index + 1;
+        wave_struct.data = [data{1, 2:length(wave_struct.wavelength)+1}];
+    else
+        warning('A line start with "Wave Length" was not found.');
+    end
+    if exist('column_index_of_mark', 'var') && ~isempty(column_index_of_mark)
+        wave_struct.stim = data{:, column_index_of_mark};
+    else
+        warning('A column name "Mark" was not found in a line start with Probe."');
+    end
+    index = index + 1;
 end
 fclose(fid);
 fprintf(' loaded.\n');
